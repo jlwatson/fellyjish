77f685f793a1b166618e56daefafd2efcee17858
Jean-Luc Watson <jlwatson@stanford.edu>
2018-04-27 23:26:23 -0700

    still that weird gave up error

--- pox/pox/ext/build_topology.py
+++ pox/pox/ext/build_topology.py
@@ -13,31 +13,33 @@ from pox.ext.jelly_pox import JELLYPOX
 from subprocess import Popen
 from time import sleep, time
 
+
 class JellyFishTop(Topo):
-    ''' TODO, build your topology here'''
+
     def build(self):
 
-            leftHost = self.addHost( 'h1' )
-            rightHost = self.addHost( 'h2' )
-            leftSwitch = self.addSwitch( 's3' )
-            rightSwitch = self.addSwitch( 's4' )
+        leftHost = self.addHost('h1')
+        rightHost = self.addHost('h2')
+        leftSwitch = self.addSwitch('s3')
+        rightSwitch = self.addSwitch('s4')
 
-            # Add links
-            self.addLink( leftHost, leftSwitch )
-            self.addLink( leftSwitch, rightSwitch )
-            self.addLink( rightSwitch, rightHost )
+        self.addLink(leftHost, leftSwitch)
+        self.addLink(leftSwitch, rightSwitch)
+        self.addLink(rightSwitch, rightHost)
 
 
 def experiment(net):
-        net.start()
-        sleep(3)
-        net.pingAll()
-        net.stop()
+    net.start()
+    sleep(3)
+    net.pingAll()
+    net.stop()
+
 
 def main():
 	topo = JellyFishTop()
 	net = Mininet(topo=topo, host=CPULimitedHost, link = TCLink, controller=JELLYPOX)
 	experiment(net)
+
 
 if __name__ == "__main__":
 	main()
