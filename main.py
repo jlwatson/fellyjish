77618d37c3ac6fd0a03d93b9312ca77912535ded
Jean-Luc Watson <jlwatson@stanford.edu>
2018-05-04 12:32:07 +0000

    figure fully generated

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import networkx as nx
 import pickle
-import pprint
 import random
 import sys
 from collections import defaultdict, OrderedDict
@@ -11,6 +10,7 @@ from time import sleep, time
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt 
+
 from topology import generate_topology
 
 
@@ -65,7 +65,7 @@ def is_switch_node(node_name):
     return node_name[0] == 's'
 
 
-def generate_path_counts(topo, algorithm, k, max_paths_on_link):
+def generate_path_counts(topo, algorithm, k, target_paths_on_link):
     sys.stdout.write("Generating paths using %s (k=%d).." % (algorithm, k))
     sys.stdout.flush()
 
@@ -93,9 +93,8 @@ def generate_path_counts(topo, algorithm, k, max_paths_on_link):
         for p in paths:
             for i in range(len(p)-1):
                 link_paths[(p[i], p[i+1])] += 1
-        sys.stdout.write(str(max(link_paths.values())))
-        sys.stdout.flush()
-        if max(link_paths.values()) <= max_paths_on_link:
+
+        if max(link_paths.values()) == target_paths_on_link:
             break
 
     sys.stdout.write(" done\n")
@@ -103,7 +102,24 @@ def generate_path_counts(topo, algorithm, k, max_paths_on_link):
     return link_paths
 
 
-def generate_figure(cumulative_counts, colors, out_filepath):
+def summarize(link_paths):
+    max_paths = max(link_paths.values())
+
+    # Summarize data for graph
+    link_counts = [0] * (max_paths + 1)
+    for path_count in link_paths.values():
+        link_counts[path_count] += 1
+
+    current_sum = link_counts[0]
+    cumulative_link_counts = [current_sum]
+    for i in range(1, len(link_counts)):
+        current_sum += link_counts[i]
+        cumulative_link_counts.append(current_sum)
+
+    return cumulative_link_counts
+
+
+def generate_figure(cumulative_counts, format_options, out_filepath):
 
     sys.stdout.write("Creating Figure 9...")
     sys.stdout.flush()
@@ -113,29 +129,31 @@ def generate_figure(cumulative_counts, colors, out_filepath):
 
     max_length = max([len(c) for c in cumulative_counts])
     
-    # TODO: change this for multiple count vectors
-    x1 = cumulative_counts[0]
-    y1 = range(len(cumulative_counts[0]))
+    x = cumulative_counts
+    y = [range(len(cc)) for cc in cumulative_counts]
 
-    # TODO: change this for multiple colors
-    c1 = colors[0]
+    for i in range(len(cumulative_counts)):
+        ax.step(x[i], y[i], **format_options[i])
 
-    ax.step(x1, y1, linewidth=4, color=c1)  
     plt.title("Jellyfish - Figure 9")
     plt.xlabel("Rank of Link")
     plt.ylabel("# Distinct Paths Link is on")
     plt.xticks(range(0, 3000+1, 500))
     plt.yticks(range(0, max_length+1, 2))
-    plt.axis([0, 3003, 0, max_length+0.05])
+    plt.axis([0, 3003, 0, max_length])
+    plt.legend(loc=2)
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
     plt.grid(linestyle='dotted')
+
     sys.stdout.write(" done\n")
     sys.stdout.flush()
 
     sys.stdout.write("Saving figure to %s..." % out_filepath)
     sys.stdout.flush()
+
     plt.savefig(out_filepath, format="eps", dpi=1000)
+
     sys.stdout.write(" done\n")
     sys.stdout.flush()
 
@@ -158,24 +176,41 @@ if __name__ == "__main__":
             pickle.dump(topo, f)
 
     # Calculate the random permutation traffic paths across the graph
-    # link_paths = generate_path_counts(topo, 'k-shortest', 8, 18)
-    link_paths = generate_path_counts(topo, 'ecmp', 8, 10)
-    # link_paths = generate_path_counts(topo, 'ecmp', 64, 13)
-    max_paths = max(link_paths.values())
+    link_paths = []
+    link_paths.append(generate_path_counts(topo, 'k-shortest', 8, 18))
+    link_paths.append(generate_path_counts(topo, 'ecmp', 64, 13))
+    link_paths.append(generate_path_counts(topo, 'ecmp', 8, 10))
 
-    # Summarize data for graph
-    link_counts = [0] * (max_paths + 1)
-    for path_count in link_paths.values():
-        link_counts[path_count] += 1
-
-    current_sum = link_counts[0]
-    cumulative_link_counts = [current_sum]
-    for i in range(1, len(link_counts)):
-        current_sum += link_counts[i]
-        cumulative_link_counts.append(current_sum)
+    # Summarize measurements and define display options
+    cumulative_count_list = [summarize(lps) for lps in link_paths]
+    format_options = [
+        {
+            'label': '8 Shortest Paths',
+            'color': (0.204, 0.216, 0.592),
+            'linewidth': 4,
+            'solid_capstyle': 'round',
+            'solid_joinstyle': 'round',
+        },
+        {
+            'label': '64-way ECMP',
+            'color': (0.792, 0.208, 0.220),
+            'linewidth': 2,
+            'markersize': 3,
+            'dash_capstyle': 'round',
+            'dash_joinstyle': 'round',
+            'linestyle': ':',
+        },
+        {
+            'label': '8-way ECMP',
+            'color': (0.086, 0.620, 0.098),
+            'linewidth': 2,
+            'solid_capstyle': 'round',
+            'solid_joinstyle': 'round',
+        }
+    ]
 
     # Generate Figure 9
-    generate_figure([cumulative_link_counts], [(0.204, 0.216, 0.592)], args.figure9)
+    generate_figure(cumulative_count_list, format_options, args.figure9)
 
     print
     print "Reproduction script completed successfully"
