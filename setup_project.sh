46f5ac5677e724fe7f34617e65761889f67d3b46
Jean-Luc Watson <jlwatson@stanford.edu>
2018-05-09 23:10:13 +0000

    Setup script added

---
+++ setup_project.sh
@@ -0,0 +1,10 @@
+cd ..
+git clone git://github.com/mininet/mininet.git
+cd mininet
+git checkout 2.2.2
+util/install.sh -a
+cd ../fellyjish
+sudo apt install python-pip
+sudo pip install networkx
+sudo pip install matplotlib
+
